98992ab7168e31adb26be8ccbdf8eedc7e44e72f
Oleg Rovenskyi <rovenskyioleg7@gmail.com>
2017-03-01 17:24:10 +0200

    HW1: update structure

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+	selector: 'home',
+	encapsulation: ViewEncapsulation.None,
+	providers: [],
+  styleUrls: [ './home.scss' ],
+	templateUrl: './home.html'
+})
+
+export class HomeComponent implements OnInit, OnDestroy {
+	constructor() {
+		console.log('Home page constructor');
+	}
+
+	public ngOnInit() {
+		console.log('Home page init');
+	}
+
+	public ngOnDestroy() {
+		console.log('unsubscribe');
+	}
+}
