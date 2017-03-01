d310bec633ee81b97c1a72a5978822a30f765974
Oleg Rovenskyi <rovenskyioleg7@gmail.com>
2017-03-01 21:37:31 +0200

    HW1: fixed tslint

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,23 +1,23 @@
 import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
-	selector: 'home',
-	encapsulation: ViewEncapsulation.None,
-	providers: [],
+  selector: 'home',
+  encapsulation: ViewEncapsulation.None,
+  providers: [],
   styleUrls: [ './home.scss' ],
-	templateUrl: './home.html'
+  templateUrl: './home.html'
 })
 
 export class HomeComponent implements OnInit, OnDestroy {
-	constructor() {
-		console.log('Home page constructor');
-	}
+  constructor() {
+    console.log('Home page constructor');
+  }
 
-	public ngOnInit() {
-		console.log('Home page init');
-	}
+  public ngOnInit() {
+    console.log('Home page init');
+  }
 
-	public ngOnDestroy() {
-		console.log('unsubscribe');
-	}
+  public ngOnDestroy() {
+    console.log('unsubscribe');
+  }
 }
