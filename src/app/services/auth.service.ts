ad488d6bc3bbbb3513d9d3b53a037716e9df23bb
Oleg Rovenskyi <rovenskyioleg7@gmail.com>
2017-03-20 23:36:11 +0200

    HW3: fixed footer

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,10 +6,6 @@ import { UserInfo } from '../models/user.model';
 export class AuthService {
   private userInfo: UserInfo;
 
-  constructor() {
-
-  }
-
   generateId(): string {
     return _.uniqueId('id_');
   }
