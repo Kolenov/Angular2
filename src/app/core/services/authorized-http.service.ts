5b0176642285fe26027681a19afb31ec7cd1d846
Oleg Rovenskyi <rovenskyioleg7@gmail.com>
2017-04-22 16:25:57 +0300

    HW7: wrapper fot Http

--- src/app/core/services/authorized-http.service.ts
+++ src/app/core/services/authorized-http.service.ts
@@ -1,55 +1,59 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ConnectionBackend, Headers, Http, Request, RequestOptions, RequestOptionsArgs, Response } from '@angular/http';
+import {
+  ConnectionBackend, Headers, Http, Request, RequestOptions, RequestOptionsArgs, Response,
+  XHRBackend
+} from '@angular/http';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthorizedHttpService {
+export class AuthorizedHttpService extends Http {
+  constructor (authService: AuthService,
+               backend: XHRBackend,
+               options: RequestOptions) {
 
-  constructor(private http: Http, private authService: AuthService) {}
+    let token = authService.token;
 
-  createAuthorizationHeader(headers: Headers) {
-    headers.append('Authorization', this.authService.token || '');
-  }
+    options.headers.set('Authorization', `Bearer ${token}`);
 
-  get(url: string, options?: RequestOptionsArgs): Observable<Response> {
-    let headers = new Headers();
-
-    this.createAuthorizationHeader(headers);
-
-    return this.http.get(url, {
-      headers
-    });
-  }
-
-  post(url, data) {
-    let headers = new Headers();
-
-    this.createAuthorizationHeader(headers);
-
-    return this.http.post(url, data, {
-      headers
-    });
+    super(backend, options);
   }
 }
-
 // export class AuthorizedHttpService extends Http {
-//   constructor(backend: ConnectionBackend, defaultOptions: RequestOptions) {
-//     super(backend, defaultOptions);
+//
+//   constructor (backend: XHRBackend, options: RequestOptions, authService: AuthService) {
+//     let token = authService.token || '';
+//
+//     options.headers.set('Authorization', token);
+//
+//     super(backend, options);
 //   }
 //
 //   request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
-//     console.log('request...');
-//     return super.request(url, options).catch(res => {
-//       console.log('res', res);
-//       // do something
-//     });
+//     let token = '21312312';
+//
+//     if (typeof url === 'string') {
+//       if (!options) {
+//         options = { headers: new Headers() };
+//       }
+//
+//       options.headers.set('Authorization', token);
+//     } else {
+//       url.headers.set('Authorization', token);
+//     }
+//
+//     return super.request(url, options).catch(this.catchAuthError(this));
 //   }
 //
-//   get(url: string, options?: RequestOptionsArgs): Observable<Response> {
-//     console.log('get...');
-//   return super.get(url, options).catch(res => {
-//     // do something
-//   });
-// }
+//   private catchAuthError (self: AuthorizedHttpService) {
+//     return (res: Response) => {
+//       console.log(res);
+//
+//       if (res.status === 401 || res.status === 403) {
+//         console.log(res);
+//       }
+//
+//       return Observable.throw(res);
+//     };
+//   }
 // }
--- src/app/core/services/courses-resource.service.ts
+++ src/app/core/services/courses-resource.service.ts
@@ -7,66 +7,30 @@ import { AuthorizedHttpService } from './authorized-http.service';
 export class CoursesResourceService {
   private baseUrl: string;
 
-  constructor(private http: Http, private authorizedHttpService: AuthorizedHttpService) {
+  constructor(private http: Http, private authorizedHttp: AuthorizedHttpService) {
     this.baseUrl = 'http://localhost:3004';
   }
 
   getCourses(start: number, count: number): Observable<Response> {
     let requestOptions = new RequestOptions();
-    let request: Request;
     let urlParams: URLSearchParams = new URLSearchParams();
 
     urlParams.set('start', start.toString());
     urlParams.set('count', count.toString());
 
-    requestOptions.url = `${ this.baseUrl }/courses`;
-    requestOptions.method = RequestMethod.Get;
     requestOptions.search = urlParams;
 
-    request = new Request(requestOptions);
-
-    return this.http.request(request)
-      .map(this.parseResponce);
-  }
-
-  getCourses1(start: number, count: number): Observable<Response> {
-    let requestOptions = new RequestOptions();
-    let urlParams: URLSearchParams = new URLSearchParams();
-
-    urlParams.set('start', start.toString());
-    urlParams.set('count', count.toString());
-
-    requestOptions.url = `${ this.baseUrl }/courses`;
-    requestOptions.method = RequestMethod.Get;
-    requestOptions.search = urlParams;
-
-    return this.authorizedHttpService.get(`${ this.baseUrl }/courses`, requestOptions)
+    return this.authorizedHttp.get(`${ this.baseUrl }/courses`, requestOptions)
       .map(this.parseResponce);
   }
 
   getCoursesCount(): Observable<Response> {
-    let requestOptions = new RequestOptions();
-    let request: Request;
-
-    requestOptions.url = `${ this.baseUrl }/courses/count`;
-    requestOptions.method = RequestMethod.Get;
-
-    request = new Request(requestOptions);
-
-    return this.http.request(request)
+    return this.authorizedHttp.get(`${ this.baseUrl }/courses/count`)
       .map(this.parseResponce);
   }
 
   deleteCourse(id: number): Observable<Response> {
-    let requestOptions = new RequestOptions();
-    let request: Request;
-
-    requestOptions.url = `${ this.baseUrl }/courses/${ id }`;
-    requestOptions.method = RequestMethod.Delete;
-
-    request = new Request(requestOptions);
-
-    return this.http.request(request)
+    return this.authorizedHttp.delete(`${ this.baseUrl }/courses/${ id }`)
       .map(this.parseResponce);
   }
 
