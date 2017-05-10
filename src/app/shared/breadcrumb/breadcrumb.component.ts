bd0795b9cfd1c3fa677f96d6c501aaafc5c5bbf9
Oleg Rovenskyi <rovenskyioleg7@gmail.com>
2017-05-10 16:21:00 +0300

    HW11: fixed warnings

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, Output, EventEmitter, Input } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
 	selector: 'cr-breadcrumb',
@@ -7,23 +7,4 @@ import { Component, ViewEncapsulation, Output, EventEmitter, Input } from '@angu
 	encapsulation: ViewEncapsulation.None
 })
 export class CrBreadcrumbComponent {
-  // @Output() onClick: EventEmitter<void> = new EventEmitter<void>();
-  // @Input() type: string;
-  // @Input() disabled: string;
-  // @Input() buttonSize: string = 'default';
-  // @Input() buttonClass: string;
-  //
-  // getComponentClasses(): string {
-  //   const classes: string[] = [
-  //     'btn',
-  //     `cr-button--size-${ this.buttonSize }`,
-  //     this.buttonClass
-  //   ];
-  //
-  //   return classes.join(' ');
-  // }
-  //
-  // clickHandler(): void {
-	//   this.onClick.emit();
-  // }
 }
