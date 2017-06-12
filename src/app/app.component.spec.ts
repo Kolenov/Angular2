b55c2e2d7d90c63c393834d7e9d70c1a9247ddf7
Oleg Rovenskyi <rovenskyioleg7@gmail.com>
2017-06-12 18:04:15 +0300

    HW-13: add test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,10 +10,9 @@ import {
  * Load the implementations that should be tested
  */
 import { AppComponent } from './app.component';
-// import { AppState } from './app.service';
 
 describe(`App`, () => {
-  let comp: AppComponent;
+  let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
   /**
@@ -23,7 +22,6 @@ describe(`App`, () => {
     TestBed.configureTestingModule({
       declarations: [ AppComponent ],
       schemas: [NO_ERRORS_SCHEMA],
-      // providers: [AppState]
     })
     /**
      * Compile template and css
@@ -36,7 +34,7 @@ describe(`App`, () => {
    */
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
-    comp    = fixture.componentInstance;
+    component = fixture.componentInstance;
 
     /**
      * Trigger initial data binding
@@ -46,21 +44,6 @@ describe(`App`, () => {
 
   it(`should be readly initialized`, () => {
     expect(fixture).toBeDefined();
-    expect(comp).toBeDefined();
+    expect(component).toBeDefined();
   });
-
-  // it(`should be @AngularClass`, () => {
-  //   expect(comp.url).toEqual('https://twitter.com/AngularClass');
-  //   expect(comp.angularclassLogo).toEqual('assets/img/angularclass-avatar.png');
-  //   expect(comp.name).toEqual('Angular 2 Webpack Starter');
-  // });
-  //
-  // it('should log ngOnInit', () => {
-  //   spyOn(console, 'log');
-  //   expect(console.log).not.toHaveBeenCalled();
-  //
-  //   comp.ngOnInit();
-  //   expect(console.log).toHaveBeenCalled();
-  // });
-
 });
